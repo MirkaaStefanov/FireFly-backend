4cac88c7e8f966501530d700d41b9067c0ce9dc7
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-03-31 20:30:38 +0300

    change

--- src/main/java/com/example/FireFly_backend/repositories/FinalProductNeedRepository.java
+++ src/main/java/com/example/FireFly_backend/repositories/FinalProductNeedRepository.java
@@ -1,8 +1,9 @@
 package com.example.FireFly_backend.repositories;
 
+import com.example.FireFly_backend.models.entity.FinalProductNeed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FinalProductNeedRepository extends JpaRepository<FinalProductNeedRepository, Long> {
+public interface FinalProductNeedRepository extends JpaRepository<FinalProductNeed, Long> {
 }
